7efb51b55621b9bf18a8b2b657c5de6652d75ccf
Lee Do Yeon <to2395@gmail.com>
2017-11-06 13:40:24 +0900

    .

--- Project/MainGame.py
+++ Project/MainGame.py
@@ -4,8 +4,8 @@ from pico2d import *
 # fill state here
 import start_state
 #import title_state
-import main_state
+#import main_state
 
-open_canvas(1000,600)
-game_framework.run(main_state)
-close_canvas()
+#open_canvas(1000,600)
+game_framework.run(start_state)
+#close_canvas()
